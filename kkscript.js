7dd4d3293de83ec771718ef496daf53c2db32b24
jonyi guo <ji394su311203106@gmail.com>
2016-02-02 18:11:13 +0800

    更改
    顏色新增

--- kkscript.js
+++ kkscript.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
 	$('.red').mouseout(function(){
 		$('body').css("background-color","white");
-	});
+	});//red
 
 
 	$('.orange').mouseover(function(){
@@ -18,7 +18,7 @@ $(document).ready(function(){
 	$('.orange').mouseout(function(){
 		$('body').css("background-color","white");
 	});
-
+//orange
 
 	$('.yellow').mouseover(function(){
 		$('body').css("background-color","yellow");
@@ -29,4 +29,43 @@ $(document).ready(function(){
 		$('body').css("background-color","white");
 	});
 
+//yellow
+	$('.green').mouseover(function(){
+		$('body').css("background-color","green");
+
+	});
+
+	$('.green').mouseout(function(){
+		$('body').css("background-color","white");
+	});
+//blue
+	$('.blue').mouseover(function(){
+		$('body').css("background-color","blue");
+
+	});
+
+	$('.blue').mouseout(function(){
+		$('body').css("background-color","white");
+	});
+
+//indigo
+	$('.indigo').mouseover(function(){
+		$('body').css("background-color","indigo");
+
+	});
+
+	$('.indigo').mouseout(function(){
+		$('body').css("background-color","white");
+	});
+
+	//cyan
+
+	$('.cyan').mouseover(function(){
+		$('body').css("background-color","cyan");
+
+	});
+
+	$('.cyan').mouseout(function(){
+		$('body').css("background-color","white");
+	});
 });
