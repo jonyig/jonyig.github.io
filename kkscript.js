074430a599fdd474a8e22c0f6868fb78fb72dd1c
jonyi guo <ji394su311203106@gmail.com>
2016-02-02 17:44:35 +0800

    更改頁面

---
+++ kkscript.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+	$('.red').mouseover(function(){
+		$('body').css("background-color","red");
+
+	});
+
+	$('.red').mouseout(function(){
+		$('body').css("background-color","white");
+	});
+
+
+	$('.orange').mouseover(function(){
+		$('body').css("background-color","orange");
+
+	});
+
+	$('.orange').mouseout(function(){
+		$('body').css("background-color","white");
+	});
+
+
+	$('.yellow').mouseover(function(){
+		$('body').css("background-color","yellow");
+
+	});
+
+	$('.yellow').mouseout(function(){
+		$('body').css("background-color","white");
+	});
+
+});
